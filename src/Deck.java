400bb81fb8e55ad78fe4ea9f1697966771253b6f
elijah <elijah.chndler@menloschool.org>
2022-12-01 14:58:56 -0800

    h

---
+++ src/Deck.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class Deck
+{
+    private int cardsleft;
+    private ArrayList<card> stack = new ArrayList<card>();
+    public Deck(int[] rank, String[] suit,int[] point)
+    {
+  
+    }
+}
