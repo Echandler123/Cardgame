86ee6e3f4bafdf3a2f4d0d0059a05ad593b173ab
elijah <elijah.chndler@menloschool.org>
2022-12-08 22:40:29 -0800

    h

--- src/Card.java
+++ src/Card.java
@@ -3,6 +3,7 @@ public class Card
     private String rank;
     private String suit;
     private int point;
+    //Constructor for the card class takes in the value of the rank suit and point of the card the is being created
     public Card(String rank, String suit, int point)
     {
         this.rank = rank;
@@ -10,35 +11,37 @@ public class Card
         this.point = point;
     }
 
+    //method that returns the value of the point of this card
     public int getPoint()
     {
         return point;
     }
-
+    //method that returns the value of the rank of this card
     public String getRank()
     {
         return rank;
     }
-
+    //method that returns the suit of this card
     public String getSuit()
     {
         return suit;
     }
-
+    //method that allows you to choose a rank to input for this card
     public void setRank(String rank)
     {
         this.rank = rank;
     }
-
+    //method that allows you to choose a point to input for this card
     public void setPoint(int point)
     {
         this.point = point;
     }
-
+    //method that allows you to choose a suit to input for this card
     public void setSuit(String suit)
     {
         this.suit = suit;
     }
+    //method that prints out what the rank and suit of the card is
     public String toString()
 
     {
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+//Elijah Chandler
 public class Main {
     public static void main(String[] args)
     {
+        //creates one game
         Game one = new Game("Elijah");
+        //plays one game
         one.playGame();
     }
 }
