8e0a4446d89327ef3058f299c3e1a53a06527f7f
elijah <elijah.chndler@menloschool.org>
2022-12-09 14:04:11 -0800

    h

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@
 //10/8/22
 import java.util.Scanner;
 public class Main {
-    //the main method asks for the user to input their name and will run the game
+    //the main method plays one game and asks for the user to input their name and will run the game after
+    // the game finishes it asks if you want to play another
     public static void main(String[] args)
     {
         String choice = "Y";
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name:");
         String name = sc.nextLine();
-        //creates one game
+
         while(choice.equals("Y") || choice.equals("y") ) {
             Game one = new Game(name);
             one.playGame();
